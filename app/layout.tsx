a8f5b8aff0b8bbddee21200b56b319592aef326b
Jacob Ma <89415176+Jacob-SouthernCity@users.noreply.github.com>
2025-03-02 15:47:34 -0500

    fix: tab title fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ricardo | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  title: "Jingze Ma | Personal Portfolio",
+  description: "Jingze Ma is a software engineer and game developer.",
 };
 
 export default function RootLayout({
